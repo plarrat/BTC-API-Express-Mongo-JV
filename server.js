9d417526a4d73f0c2b30e55b67b1deb469478cab
plarrat <larrat.philippe@lessonsharing.fr>
2022-01-25 14:31:15 +0100

    Gestion Cors

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 const db = mongoose.connect('mongodb://localhost/db_jv_btc', {
   useNewUrlParser: true,
 })
 
-mongoose.connection.on('open', () => console.log('Connexion réussie'))
+mongoose.connection.on('open', () => console.log('Connexion réussie 😁'))
 
 const Constructeurs = require('./schemas/constructeurs.schemas')
 
 const server = express()
+
+server.use(cors())
 
 server.get('/', (req, res) => {
   res.writeHead(200)
@@ -17,8 +20,15 @@ server.get('/', (req, res) => {
 })
 
 server.get('/constructeurs', (req, res) => {
-  res.writeHead(200)
-  res.end('<h1>Bienvenue sur notre API</h1>')
+  Constructeurs.find({}, (err, constructeurs) => {
+    if (err) {
+      console.log('Erreur lors de la récupération des constructeurs')
+      console.log(err)
+    } else {
+      console.log(constructeurs)
+      res.json(constructeurs)
+    }
+  })
 })
 
 server.listen(3000)
