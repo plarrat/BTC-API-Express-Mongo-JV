a8524a33d249a3ffc2fe76fc253aab48e07056c2
plarrat <larrat.philippe@lessonsharing.fr>
2022-01-25 12:26:18 +0100

    Premier Schemas

--- server.js
+++ server.js
@@ -7,9 +7,16 @@ const db = mongoose.connect('mongodb://localhost/db_jv_btc', {
 
 mongoose.connection.on('open', () => console.log('Connexion réussie'))
 
+const Constructeurs = require('./schemas/constructeurs.schemas')
+
 const server = express()
 
 server.get('/', (req, res) => {
+  res.writeHead(200)
+  res.end('<h1>Bienvenue sur notre API</h1>')
+})
+
+server.get('/constructeurs', (req, res) => {
   res.writeHead(200)
   res.end('<h1>Bienvenue sur notre API</h1>')
 })
