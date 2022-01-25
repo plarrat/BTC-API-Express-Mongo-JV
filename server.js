425b90e0dc142c7392f3b8115d629d2da62f6863
plarrat <larrat.philippe@lessonsharing.fr>
2022-01-25 16:47:28 +0100

    API constructeurs

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
+const bodyParser = require('body-parser')
 
 const db = mongoose.connect('mongodb://localhost/db_jv_btc', {
   useNewUrlParser: true,
@@ -13,6 +14,8 @@ const Constructeurs = require('./schemas/constructeurs.schemas')
 const server = express()
 
 server.use(cors())
+server.use(bodyParser.urlencoded({ extended: false }))
+server.use(bodyParser.json())
 
 server.get('/', (req, res) => {
   res.writeHead(200)
@@ -29,6 +32,43 @@ server.get('/constructeurs', (req, res) => {
       res.json(constructeurs)
     }
   })
+})
+
+//POST == Ajout
+server.post('/constructeurs/add', (req, res) => {
+  Constructeurs.create(req.body)
+  res.writeHead(201)
+  res.end()
+})
+
+//DELETE == Suppression
+server.delete('/constructeurs/:id', (req, res) => {
+  const id = req.params.id
+  Constructeurs.deleteOne({ _id: id }, err => {
+    console.log('erreur suppression')
+    console.log(err)
+    // res.writeHead(409)
+  })
+  res.writeHead(205)
+  res.end()
+})
+
+// PUT == Modification
+server.put('/constructeurs/:id', (req, res) => {
+  const id = req.params.id
+  const elem = req.body
+  Constructeurs.updateOne({ _id: id }, elem, (err, infos) => {
+    if (err) console.log('Erreur de modification')
+    res.writeHead(200)
+    res.end()
+  })
+})
+
+// Récupérer un document dans la base via son ID
+server.get('/constructeurs/:id', async (req, res) => {
+  const id = req.params.id
+  let elem = await Constructeurs.findById(id)
+  res.json(elem)
 })
 
 server.listen(3000)
